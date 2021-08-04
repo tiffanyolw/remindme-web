715c0dfd4e5887cd9d5adaa694de0590edb32ad6
twong4 <62853188+twong4@users.noreply.github.com>
2021-08-04 18:36:07 -0400

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: "https://app-remind-me.herokuapp.com" //"http://localhost:3000"
 };
 
 /*
